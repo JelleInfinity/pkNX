c4bac6ece181ef3df55ccb66b428ea95997ccb5c
Maurizio Vitale <mauriziovit@gmail.com>
2022-10-05 23:41:20 -0400

    Expand on Throw Flatbuffers

--- pkNX.Structures.FlatBuffers/Arceus/Throw/ThrowableParamTable8a.cs
+++ pkNX.Structures.FlatBuffers/Arceus/Throw/ThrowableParamTable8a.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.ComponentModel;
 using FlatSharp.Attributes;
 
@@ -22,14 +22,14 @@ public class ThrowableParamTable8a : IFlatBufferArchive<ThrowableParam8a>
 [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
 public class ThrowableParam8a
 {
-    [FlatBufferItem(00)] public int    ItemID { get; set; }
-    [FlatBufferItem(01)] public ulong  Hash_01 { get; set; }
-    [FlatBufferItem(02)] public ulong  ThrowPermissionSet  { get; set; }
-    [FlatBufferItem(03)] public ulong  ThrowParam          { get; set; }
-    [FlatBufferItem(04)] public ulong  Hash_04 { get; set; }
-    [FlatBufferItem(05)] public int    Field_05 { get; set; }
-    [FlatBufferItem(06)] public float  Field_06 { get; set; }
+    [FlatBufferItem(00)] public int ItemID { get; set; }
+    [FlatBufferItem(01)] public ThrowItemType8a ItemType { get; set; }
+    [FlatBufferItem(02)] public ThrowPermissionSetType8a ThrowPermissionSet { get; set; }
+    [FlatBufferItem(03)] public ThrowParamType8a ThrowParam { get; set; }
+    [FlatBufferItem(04)] public ulong Hash_04 { get; set; }
+    [FlatBufferItem(05)] public int Field_05 { get; set; }
+    [FlatBufferItem(06)] public float Field_06 { get; set; }
     [FlatBufferItem(07)] public string Label_01 { get; set; } = string.Empty;
     [FlatBufferItem(08)] public string Label_02 { get; set; } = string.Empty;
-    [FlatBufferItem(09)] public float  Field_09 { get; set; }
+    [FlatBufferItem(09)] public float Field_09 { get; set; }
 }
